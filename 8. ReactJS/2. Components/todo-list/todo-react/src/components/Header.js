23f80674e5ec577e74ef4d140b5d61eb0293efd4
dido-r <deyan_r@abv.bg>
2023-08-09 01:22:03 +0300

    Delete 8. ReactJS directory

--- 8. ReactJS/2. Components/todo-list/todo-react/src/components/Header.js
+++
@@ -1,8 +0,0 @@
-export function Header() {
-    return (
-        <header className="navigation-header">
-            <span className="spacer"></span>
-            <span className="navigation-description">Todo List</span>
-        </header>
-    );
-}
